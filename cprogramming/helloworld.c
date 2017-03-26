58c50b41d3859dcc1623de79ae3a2a858d958a45
Ravi Kiran <kiran.talluri89@gmail.com>
2017-03-26 07:16:52 +0530

    C Programming: First Hello World Program.

---
+++ cprogramming/helloworld.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main() //int represents main will return some integer value
+{
+  printf("Hello world"); // every C statement ends with semi colon
+  return 0; // here we are returning some integer
+}
